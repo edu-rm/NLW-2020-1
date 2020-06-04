0d12fdc2d726cec7b3f7b326220de1122b0ec6e9
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-03 21:32:24 -0300

    images

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
 
-routes.get('/', (req, res) => {
-    return res.json({ msg: 'working'});
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  return res.json(items);
 });
 
 
